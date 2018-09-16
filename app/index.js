9dcc70ac64e76a6c454dbea2b2f945137b710fde
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2018-09-16 10:59:58 -0400

    Updated app code.

--- app/index.js
+++ app/index.js
@@ -2,9 +2,10 @@
  * Entry point for the watch app
  */
 import document from "document";
+import { inbox } from "file-transfer"
+import { unlinkSync } from "fs";
 
 let demotext = document.getElementById("mainTitle");
-demotext.text = "KnocKnocK";
 let personPic = document.getElementById("pic");
 let whenI = document.getElementById("whenI");
 let whereI = document.getElementById("whereI");
@@ -25,6 +26,39 @@ function fetchKnocks() {
 // Display the knock data received from the companion
 function processKnockData(data) {
   //console.log("The temperature is: " + data.temperature);
+  let VTList = document.getElementById("my-list");
+  console.log(JSON.stringify(data));
+  var numEle = 0;
+  var arr = Object.keys(data).map(function(k) {
+    numEle++;
+    return data[k];
+  });
+  let NUM_ELEMS = numEle;
+  
+  var cEle = 0;
+  VTList.delegate = {
+    getTileInfo: function(index) {
+      return {
+        type: "my-pool",
+        value: "Menu item",
+        index: index
+      };
+    },
+    configureTile: function(tile, info) {
+      if (info.type == "my-pool") {
+        tile.getElementById("whoI").text = `Who: ${arr[cEle].gender}, ${arr[cEle].age_range}`;
+        tile.getElementById("whenI").text = `When: ${arr[cEle].time}`;
+        tile.getElementById("pic").href = `/private/data/face${cEle}.jpg`;
+        let touch = tile.getElementById("touch-me");
+        touch.onclick = evt => {
+          console.log(`touched: ${info.index}`);
+        };
+        cEle++;
+      }
+    }
+  };
+  // VTList.length must be set AFTER VTList.delegate
+  VTList.length = NUM_ELEMS;
 }
 
 // Listen for the onopen event
@@ -46,5 +80,18 @@ messaging.peerSocket.onerror = function(err) {
   console.log("Connection error: " + err.code + " - " + err.message);
 }
 
+function processAllFiles() {
+  let fileName;
+  while (fileName = inbox.nextFile()) {
+    console.log(`/private/data/${fileName} is now available`);
+  }
+  let vt = document.getElementById("my-list");
+  //console.log(vt.length);
+}
+
 // Fetch the knock every 60 seconds
-setInterval(fetchKnocks, 1000 * 60);
+setInterval(fetchKnocks, 1000 * 15);
+
+//Check for newly downloaded files.
+inbox.addEventListener("newfile", processAllFiles);
+processAllFiles();
--- companion/index.js
+++ companion/index.js
@@ -6,16 +6,17 @@ console.log("Companion code started");
 
 // Import the messaging module
 import * as messaging from "messaging";
+//import { unlinkSync } from "fs";
 
 var URL = "https://hack-the-north-2018.firebaseio.com/users/";
-var user = "iUTwfKCMbsMkw0RQYN7Vnfy9sjI3";
+var user = "9DZklrkXnYYGhtjTgb57ViG68Vn1";
 function queryKnocks() {
   fetch(URL+user+".json")
   .then(function (response) {
-    console.log(response);
+//    console.log(response);
       response.json()
       .then(function(data) {
-        console.log(JSON.stringify(data));
+//        console.log(JSON.stringify(data));
         // Send the knock data to the device
         returnKnockData(data);
       });
@@ -30,6 +31,7 @@ function returnKnockData(data) {
   if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
     // Send a command to the device
     messaging.peerSocket.send(data);
+    fetchImgs(data);
   } else {
     console.log("Error: Connection is not open");
   }
@@ -47,4 +49,48 @@ messaging.peerSocket.onmessage = function(evt) {
 messaging.peerSocket.onerror = function(err) {
   // Handle any errors
   console.log("Connection error: " + err.code + " - " + err.message);
+}
+
+
+
+// Import Outbox from the file-transfer module
+import { outbox } from "file-transfer"
+
+function fetchImg(url, fn)
+{
+  // Source image on the internet
+  let srcImage = url;
+
+  // Destination filename
+  let destFilename = fn;
+  
+  // Fetch the image from the internet
+  fetch(srcImage).then(function (response) {
+    // We need an arrayBuffer of the file contents
+    return response.arrayBuffer();
+  }).then(function (data) {
+    // Queue the file for transfer
+    outbox.enqueue(destFilename, data).then(function (ft) {
+      // Queued successfully
+      console.log("Transfer of '" + destFilename + "' successfully queued.");
+    }).catch(function (error) {
+      // Failed to queue
+      throw new Error("Failed to queue '" + destFilename + "'. Error: " + error);
+    });
+  }).catch(function (error) {
+    // Log the error
+    console.log("Failure: " + error);
+  });
+}
+
+function fetchImgs(data){
+  var arr = Object.keys(data).map(function(k) {
+    return data[k];
+  });
+  console.log(arr.length);
+  for (var i=0; i<arr.length; i++){
+    //unlinkSync("/private/data/face"+i+".jpg");
+    fetchImg("https://firebasestorage.googleapis.com/v0/b/hack-the-north-2018.appspot.com/o/images%2F9DZklrkXnYYGhtjTgb57ViG68Vn1%2Fface"+i+".jpg?alt=media&token=04dc75cc-04a7-46a1-85dc-c556cae05eb0", "face"+i+".jpg");
+  }
+  
 }
